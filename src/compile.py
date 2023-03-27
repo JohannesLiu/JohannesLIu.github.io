e930c4b870270fb2267d07e1189697e8cdc0ac6c
johannesliu <johannesliu@live.com>
2023-03-27 11:28:55 +0800

    update

--- src/compile.py
+++ src/compile.py
@@ -5,12 +5,12 @@ compile_list = ["index.jemdoc",
                 "news.jemdoc",
                 "skills.jemdoc",
                 "talks.jemdoc",
-                "mics.jemdoc",
+                "edge-music.jemdoc",
                 "papers.jemdoc",
                 # "books.jemdoc",
                 "awards.jemdoc",
                 "service.jemdoc",
-                "mylab.jemdoc"
+                "edge-hsc.jemdoc"
                 ]
 
 os.system("python jemdoc -c mysite.conf " + " ".join(compile_list))
